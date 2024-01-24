563ae4427510bd172797b7c6f1cc9649f5e6abf7
phy-lei <765373325@qq.com>
2024-01-24 16:39:15 +0800

    chore: 新增lock.json

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
-import { PropsWithChildren } from 'react'
-// import { useLaunch } from '@tarojs/taro'
 import './app.css'
 
-function App({ children }: PropsWithChildren<any>) {
-
-  // useLaunch(() => {
-  //   console.log('App launched.')
-  // })
-
+function App({ children }) {
   // children 是将要会渲染的页面
   return children
 }
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,15 @@
 import { View, Text, Button } from "@tarojs/components";
-import Taro from "@tarojs/taro";
 import { createSignal } from "solid-js";
 import "./index.css";
-import { useLoad } from "@tarojs/taro";
 
 export default function Index() {
   const [count, setCount] = createSignal(0);
   const [color, setColor] = createSignal('red')
 
   const handleClick = () => {
-    console.log('%c [ xxx ]', 'font-size:13px; background:pink; color:#bf2c9f;', Taro);
     setCount(count() + 1)
-    Taro.navigateTo({
-      url: '/pages/about/index'
-    })
-  }
 
-  useLoad(() => {
-    console.log('%c [ xxx ]', 'font-size:13px; background:pink; color:#bf2c9f;', 123);
-  })
+  }
 
   return (
     <View className="index">
